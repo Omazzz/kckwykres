fbd2db668efae0015c6b90ef6615b83648906c0e
Omazzz <44099820+Omazzz@users.noreply.github.com>
2018-10-29 21:22:30 +0100

    Update Controller.java

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,6 @@
 package sample;
 
-import javafx.application.Application;
 import javafx.fxml.FXML;
-import javafx.print.PrinterJob;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
@@ -26,31 +24,74 @@ public class Controller {
 
     //right side
     @FXML
-    CategoryAxis miesiacedane;
+    CategoryAxis xAxis;
     @FXML
-    NumberAxis zarobkidane;
+    NumberAxis yAxis;
     @FXML
     BarChart<String,String> wykres;
     @FXML
     TableView<MiesięcznyZarobek> tabela;
 
+
+
+
+
     Boolean isInitialized = false;
     XYChart.Series series1 = new XYChart.Series();
+    public void FillIn(){
+
+
+        series1.getData().add(new XYChart.Data("Styczeń",Double.parseDouble(String.valueOf(5.0))));
+        MiesięcznyZarobek m = new MiesięcznyZarobek("Styczeń","5000");
+        tabela.getItems().add(m);
+        series1.getData().add(new XYChart.Data("Luty",Double.parseDouble(String.valueOf(2.0))));
+        MiesięcznyZarobek m1 = new MiesięcznyZarobek("Luty","2000");
+        tabela.getItems().add(m1);
+        series1.getData().add(new XYChart.Data("Marzec",Double.parseDouble(String.valueOf(1.0))));
+        MiesięcznyZarobek m2 = new MiesięcznyZarobek("Marzec","1000");
+        tabela.getItems().add(m2);
+        series1.getData().add(new XYChart.Data("Kwiecień",Double.parseDouble(String.valueOf(2.0))));
+        MiesięcznyZarobek m3 = new MiesięcznyZarobek("Kwiecień","2000");
+        tabela.getItems().add(m3);
+        series1.getData().add(new XYChart.Data("Maj",Double.parseDouble(String.valueOf(1.0))));
+        MiesięcznyZarobek m4 = new MiesięcznyZarobek("Maj","1000");
+        tabela.getItems().add(m4);
+        series1.getData().add(new XYChart.Data("Czerwiec",Double.parseDouble(String.valueOf(3.0))));
+        MiesięcznyZarobek m5 = new MiesięcznyZarobek("Czerwiec","3000");
+        tabela.getItems().add(m5);
+        series1.getData().add(new XYChart.Data("Lipiec",Double.parseDouble(String.valueOf(2.0))));
+        MiesięcznyZarobek m6 = new MiesięcznyZarobek("Lipiec","2000");
+        tabela.getItems().add(m6);
+        series1.getData().add(new XYChart.Data("Sierpień",Double.parseDouble(String.valueOf(0.0))));
+        series1.getData().add(new XYChart.Data("Wrzesień",Double.parseDouble(String.valueOf(0.0))));
+        series1.getData().add(new XYChart.Data("Pażdziernik",Double.parseDouble(String.valueOf(0.0))));
+        series1.getData().add(new XYChart.Data("Listopad",Double.parseDouble(String.valueOf(0.0))));
+        series1.getData().add(new XYChart.Data("Grudzień",Double.parseDouble(String.valueOf(0.0))));
+
+
+
+        wykres.getData().add(series1);
+}
 
     public void initialize() {
         miesiac.setCellValueFactory(new PropertyValueFactory<>("miesiac"));
         zarobki.setCellValueFactory(new PropertyValueFactory<>("zarobki"));
         isInitialized = true;
+        FillIn();
     }
-
 
     public void uzupenij(){
         String mies=miesiactext.getText();
         String zarob=zarobkitext.getText();
-        Number zarobNumber = -10;
+        String zarob2;
+        Number zarobNumber = -1;
+        double Zarobki= Double.valueOf(zarob);
+        Zarobki=Zarobki/1000;
+        zarob2=Double.toString(Zarobki);
+
 
         try{
-            zarobNumber = Double.parseDouble(zarob);
+            zarobNumber = Double.parseDouble(zarob2);
         }catch(NumberFormatException e){
             System.out.println("To nie jest double!");
         }
@@ -60,16 +101,12 @@ public class Controller {
             initialize();
         }
 
-        System.out.println(mies);
-        System.out.println(zarob);
-        System.out.println(zarobNumber);
-        System.out.println("xd: " + mies.length());
-
         if(zarobNumber.doubleValue()>=0 && mies.length()>0){
+
+            setGraph(mies,zarobNumber);
             MiesięcznyZarobek m = new MiesięcznyZarobek(mies,zarob);
             tabela.getItems().add(m);
 
-            setGraph(mies,zarobNumber);
         }
 
         miesiactext.clear();
@@ -79,9 +116,9 @@ public class Controller {
 
     public void setGraph(String mies, Number zarob){
 
-
         series1.getData().add(new XYChart.Data(mies,zarob));
         wykres.getData().add(series1);
+
     }
 
 }
